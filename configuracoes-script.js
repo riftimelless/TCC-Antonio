fb9a533ee357b12b871525f5ae55b8047ab106e4
Esdras Berehulka <esdras.berehulka.silva@escola.pr.gov.br>
2026-05-12 09:48:59 -0300

    Add files via upload

--- configuracoes-script.js
+++ configuracoes-script.js
@@ -1,25 +1,6 @@
-const darkModeToggle = document.getElementById("darkModeToggle");
-
-// Carregar estado do dark mode
-if (localStorage.getItem("darkMode") === "true") {
-  document.body.classList.add("dark-mode");
-  darkModeToggle.checked = true;
-}
-
-// Ativar/Desativar Modo Escuro
-darkModeToggle.addEventListener("change", () => {
-  if (darkModeToggle.checked) {
-    document.body.classList.add("dark-mode");
-    localStorage.setItem("darkMode", "true");
-  } else {
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("darkMode", "false");
-  }
-});
-
 // Limpar dados
 document.getElementById("clearData").addEventListener("click", () => {
-  if (confirm("Tem certeza que deseja limpar TODOS os dados do aplicativo?")) {
+  if (confirm("Tem certeza que deseja limpar TODOS os dados do app?")) {
     localStorage.clear();
     alert("✅ Todos os dados foram limpos!");
     window.location.href = "index.html";
